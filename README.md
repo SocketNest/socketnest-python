4de63af12ae35ab0f45aa17e08fe28421c2922ba
realdanielmlozano <daniel@danielmlozano.com>
2025-04-15 18:53:10 -0600

    Base files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # socketnest-python
 SocketNest Server Python Library
+
+## Installation
+
+```bash
+pip install socketnest-python
+# or with poetry
+poetry add socketnest_python
+```
+
+## Usage
+
+```python
+import socketnest_python
+# Your usage here
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="socketnest_python",
+    version="0.1.0",
+    description="SocketNest Server Python Library",
+    author="Daniel Mendoza",
+    author_email="daniel@socketnest.com",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests>=2.0.0"
+    ],
+    tests_require=[
+        "pytest>=7.0"
+    ],
+    python_requires=">=3.7,<4.0",
+    include_package_data=True,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/socketnest/socketnest-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
